33e6a5490e9662939b4477af51b4052fcc296aca
RedCrafter07 <gh@r07.dev>
2022-11-12 13:19:39 +0100

    Add comments and error handling

--- src/lib/fileParser.ts
+++ src/lib/fileParser.ts
@@ -1,10 +1,11 @@
 import { readFile } from 'fs/promises';
 import axios from 'axios';
+import chalk from 'chalk';
 
 const cwd = process.cwd();
 
 export async function read(path: string) {
-	const file = await readFile(`${cwd}/${path}`, 'utf-8');
+	const file = await readFile(path, 'utf-8');
 	return file;
 }
 
@@ -21,7 +22,11 @@ mainFile: src/index.ts
 
 export async function parseFile(path: string) {
 	const file = await read(path);
-	let lines: string[] = file.split('\n').map((l) => l.trim().replace('\r', ''));
+
+	let lines: string[] = file
+		.split('\n')
+		.map((l) => l.trim().replace('\r', ''))
+		.filter((l) => !l.startsWith('# '));
 
 	if (lines[0].startsWith('pullFrom: ')) {
 		const file = lines[0].slice('pullFrom:'.length).trim();
@@ -34,10 +39,34 @@ export async function parseFile(path: string) {
 	const packages = lines[0].split(', ');
 	const sliceAmount = lines[1].length > 1 ? 1 : 2;
 	const config = lines.slice(sliceAmount).reduce((acc, line) => {
-		const [key, value] = line.split(': ');
+		const [key, value] = line.split(':').map((l) => l.trim());
 		acc[key] = value;
 		return acc;
 	}, {} as Record<string, string>);
 
-	console.log(packages, config);
+	const { language, packageManager, mainFile, ...additionalConfig } = config;
+
+	if (!language || !packageManager || !mainFile) {
+		if (!language) console.log(chalk.red('[!] Language not specified'));
+		if (!packageManager)
+			console.log(chalk.red('[!] Package manager not specified'));
+		if (!mainFile) console.log(chalk.red('[!] Main file not specified'));
+
+		console.log(chalk.red("[!] Can't continue without these values"));
+		console.log(
+			chalk.yellow('[/] You may configure these values in the config file.'),
+		);
+
+		process.exit(1);
+	}
+
+	return {
+		packages,
+		config: {
+			language,
+			packageManager,
+			mainFile,
+			additionalConfig,
+		},
+	};
 }
